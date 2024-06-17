c5a8ca5d136c22398adde3f06c675b59a82c49da
anshika2417 <anshikakumari571@gmail.com>
2024-06-18 00:07:34 +0530

    Basic Code

--- Variables_and_Data_Types/variables.py
+++ Variables_and_Data_Types/variables.py
@@ -1,5 +1,11 @@
 name="Anshika"  #string
 age=21
 salary=257097.99
+age2=age
 print("My name is :",name)
 print("Age and salary is: ",age, "and", salary)
+print(age)
+
+print(type(name))
+print(type(age))
+print(type(salary))
