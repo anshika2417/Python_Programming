a03785554d1f511da6871568de4225439bda4f1c
anshika2417 <anshikakumari571@gmail.com>
2024-06-18 23:58:04 +0530

    Input

---
+++ Input_in_Python/input.py
@@ -0,0 +1,12 @@
+#Taking input from user and printing it
+
+#String input
+name=input("name : ")
+
+#int input
+age=int(input("age : "))
+
+#float input
+price=float(input("Price : "))
+
+print("My name is ",name,"and i'm ",age,"years old ","and price is ",price)
---
+++ Input_in_Python/practice.py
@@ -0,0 +1,9 @@
+color=input("Color of your Light : ")
+if(color=="Red"):
+    print("Stop")
+elif(color=="Yellow"):
+    print("Wait")
+elif(color=="Green"):
+    print("Go")
+else:
+    print("light is broken")
