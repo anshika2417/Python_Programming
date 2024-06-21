9d67275de3fb57cb8a9176acb3cdb8ab85bee515
anshika2417 <anshikakumari571@gmail.com>
2024-06-21 19:30:53 +0530

    string function

---
+++ String/string.py
@@ -0,0 +1,15 @@
+#slicing
+str="ApnaCollege"
+print(str[4:len(str)])
+
+str1="i am studing in lovely professional university"
+print(str1.endswith("ity"))
+
+print(str1.capitalize())
+
+print(str1.replace("lovely","Allahabad"))
+
+#print(str1.find("a"))
+print(str1.find("hello"))
+
+print(str1.count("n"))
