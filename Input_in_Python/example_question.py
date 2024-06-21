e7b7642bd73b46583fb90fb141e526fe6e80ac6c
anshika2417 <anshikakumari571@gmail.com>
2024-06-21 18:36:31 +0530

    Practice question

---
+++ Input_in_Python/example_question.py
@@ -0,0 +1,22 @@
+#WRP to find area of square
+l=int(input("Length : "))
+
+a=l*l
+print(a)
+
+#WRP to prinaverage of two floating number
+a1=float(input("Enter 1st number "))
+a2=float(input("Enter 2nd number "))
+
+c=(a1+a2)/2
+print(c)
+
+#WRP to input two int numbers and return true if a>b if not then return false
+x=int(input("Enter 1st number "))
+y=int(input("Enter 2st number "))
+
+if(x>=y):
+    print("true")
+else:
+    print("False")
+
